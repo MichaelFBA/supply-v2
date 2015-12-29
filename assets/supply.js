ef2eea96c8fa3f26291b3ff42070181e8b724c45
Michael <michael@falsebehavinganimals.com>
2015-12-29 08:42:46 +0100

    stylistic changes

--- assets/supply.js
+++ assets/supply.js
@@ -58,55 +58,13 @@ $(document).ready(function() {
       });
     });
 
-    (function($) {
-
-      /**
-       * Copyright 2012, Digital Fusion
-       * Licensed under the MIT license.
-       * http://teamdf.com/jquery-plugins/license/
-       *
-       * @author Sam Sehnert
-       * @desc A small plugin that checks whether elements are within
-       *     the user visible viewport of a web browser.
-       *     only accounts for vertical position, not horizontal.
-       */
-
-      $.fn.visible = function(partial) {
-
-        var $t = $(this),
-          $w = $(window),
-          viewTop = $w.scrollTop(),
-          viewBottom = viewTop + $w.height(),
-          _top = $t.offset().top,
-          _bottom = _top + $t.height(),
-          compareTop = partial === true ? _bottom : _top,
-          compareBottom = partial === true ? _top : _bottom;
-
-        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
-
-      };
-
-    })(jQuery);
-
-    function ballsInView(event) {
-      $(".test-area").each(function(i, el) {
-        var el = $(el);
-        if (el.visible(true)) {
-          console.log(i)
-          balls.members[i].start(showBall.extend({
-            duration: 400,
-            delay: 500,
-            ease: 'backOut'
-          }))
-        }
-      });
-    }
-
-    $(window).scroll(function(event) {
-      ballsInView(event);
+    $(".test-area").each(function(i, el) {
+      balls.members[i].start(showBall.extend({
+        duration: 400,
+        delay: 500,
+        ease: 'backOut'
+      }))
     });
-
-    ballsInView()
 
   }
 
