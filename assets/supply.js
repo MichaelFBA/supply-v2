8ad5be71c45880fae4cfa66593f90916f2822403
Michael <michael@falsebehavinganimals.com>
2016-06-27 21:55:36 +0200

    Before changing to packery

--- assets/supply.js
+++ assets/supply.js
@@ -1,1 +1,24 @@
-$(document).ready(function() {})
+$(document).ready(function() {
+
+  //video
+  var isPlaying = false;
+  var video = $("video");
+
+  video.on('click', function() {
+    if (isPlaying) {
+      $(this)[0].pause()
+      isPlaying = false;
+    } else {
+      $(this)[0].play()
+      isPlaying = true;
+    }
+  });
+
+  $("#video").on('click', function(e) {
+    if ($(e.target).is('#video')) {
+      $('video').addClass('transitionOffScreen')
+      $(this).addClass('transitionOffScreen')
+      video[0].pause()
+    }
+  })
+})
